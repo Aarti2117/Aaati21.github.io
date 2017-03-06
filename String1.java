7724f1f514206e9d878737b0ca6d284587692e4f
Aarti2117 <dhotreaarti56@gmail.com>
2017-03-06 08:54:28 -0800

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,29 @@
+package aarti;
+
+import java.util.*;
+
+public class String1 {
+
+	public static void main(String[] args) 
+	{
+			   String p= "We test coders.Give us a try?Can i try!";
+			
+			   String[] s = p.split("\\.");
+			   String[] s2 = p.split("\\?");
+			   String[] s3 = p.split("\\!");
+
+			   List<String> a = new ArrayList<String>();
+			   a.addAll(Arrays.asList(s));
+			   a.addAll(Arrays.asList(s2));
+			   a.addAll(Arrays.asList(s3));
+			 
+			  Iterator itr=a.iterator();  
+			   while(itr.hasNext()){  	   
+			    System.out.println(itr.next());  
+			   }
+			   int retval = a.size();
+			   System.out.println("Size of list = " + retval); 
+			  }  
+	}
+
+
